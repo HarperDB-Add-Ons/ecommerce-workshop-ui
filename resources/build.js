20a05ac1489cfa03439f2003427c9b9d355e77af
deliciousmonster <jaxon@deliciousmonster.com>
2024-12-04 10:14:18 -0500

    build on restart

--- resources/build.js
+++ resources/build.js
@@ -1,8 +1,5 @@
 import { execSync } from 'node:child_process';
 
-export class Build extends Resource {
-	async get() {
-		execSync(`cd ${import.meta.dirname} && npm i -save && npm run build`);
-		return { success: 'built ui' };
-	}
-}
+
+execSync(`cd ${import.meta.dirname} && npm i -save && npm run build`);
+console.log({ success: 'built ui' });
